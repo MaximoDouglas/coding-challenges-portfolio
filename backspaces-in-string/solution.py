6d6993600b166c6cb29cd77e7a5c0fd508d66168
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-06 08:55:38 -0300

    origin/master Add Backspaces in string
    -
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

---
+++ backspaces-in-string/solution.py
@@ -0,0 +1,12 @@
+def clean_string(s):
+  new_string = ''
+  b_count = 0
+  for c in s[::-1]:
+    if (c == '#'):
+      b_count += 1
+    elif (b_count == 0):
+      new_string = c + new_string
+    else:
+      b_count -= 1
+  
+  return new_string
