9aa09eaf24197849c6ee84fb05057a0e36815ca4
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-24 22:08:52 -0300

    Add 'String incrementer' challenge

---
+++ string-incrementer/solution.py
@@ -0,0 +1,23 @@
+numbers = ['0', '1', '2', '3', '4',
+          '5', '6', '7', '8', '9']
+
+def increment_string(string):
+    string_len = len(string)
+
+    if (string_len == 0):
+      return '1'
+
+    last_index = string_len
+    for i in range((string_len - 1), -1, -1):
+        if (string[i] not in numbers):
+            break
+        else:
+            last_index = i
+    
+    if (last_index == string_len):
+        return string + '1'
+    else:
+        number = int(string[last_index:]) + 1
+        nLen = len(string[last_index:])
+
+        return string[:last_index] + str(number).zfill(nLen)
