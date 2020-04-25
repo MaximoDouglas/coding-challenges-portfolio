ac5eb43add0604c357aae6aad62366c118fcace9
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-24 22:10:46 -0300

    Add 'Pete, the baker' challenge

---
+++ pete-the-baker/solution.py
@@ -0,0 +1,19 @@
+import math
+
+def cakes(recipe, available):
+    minimun = None
+    
+    if len(recipe) > len(available):
+        return 0
+    
+    for ing in recipe:
+        if (ing not in available):
+            return 0
+        if (recipe[ing] > available[ing]):
+            return 0
+        total = available[ing]/recipe[ing]
+        
+        if (minimun == None or total < minimun):
+            minimun = math.floor(total)
+    
+    return minimun
