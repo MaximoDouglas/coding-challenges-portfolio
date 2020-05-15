660ebc186e580f250134e7402cd5beb94b3d93ca
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-15 17:14:45 -0300

    Directions Reduction' challenge

---
+++ directions-reduction/solution.py
@@ -0,0 +1,24 @@
+def areOpposite(dir1, dir2):
+    bool1 = (dir1 == 'NORTH' and dir2 == 'SOUTH') or (dir1 == 'SOUTH' and dir2 == 'NORTH')
+    bool2 = (dir1 == 'EAST' and dir2 == 'WEST') or (dir1 == 'WEST' and dir2 == 'EAST') 
+    return bool1 or bool2
+
+def dirReduc(arr):
+    oposites = []
+    i = 0
+    while i < len(arr) - 1:
+        if (areOpposite(arr[i], arr[i+1])):
+            oposites.append(i)
+            oposites.append(i+1)
+            i += 2
+        else:
+            i += 1
+    
+    if (len(oposites) == 0):
+        return arr
+    else:
+        oposites.sort(reverse=True)
+        for item in oposites:
+            arr.pop(item)
+            
+        return dirReduc(arr)
