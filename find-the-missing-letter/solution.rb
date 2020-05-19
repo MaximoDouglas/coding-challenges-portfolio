78aceee1e2c2404d34fa6f43b2cc3bfa67668b54
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-18 23:03:38 -0300

    Add 'Find the missing letter' challenge

---
+++ find-the-missing-letter/solution.rb
@@ -0,0 +1,20 @@
+def get_alpha()
+    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  end
+  
+  def find_missing_letter(arr)
+    alpha       = get_alpha()
+    begin_index = alpha.index(arr[0].downcase)
+    is_up_case  = arr[0] == arr[0].upcase
+    missing     = nil
+  
+    arr.each_with_index do |item, index|
+      alpha_index = begin_index + index
+      if item.downcase != alpha[alpha_index]
+        missing = is_up_case ? alpha[alpha_index].upcase : alpha[alpha_index]
+        break
+      end
+    end
+    
+    missing
+end
