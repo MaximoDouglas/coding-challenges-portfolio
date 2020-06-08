b8419648651314e194538c0364e0328bf425e273
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-08 20:07:34 -0300

    Add 'Vasya - Clerk' challenge

---
+++ ruby/vasya-clerk/solution.rb
@@ -0,0 +1,30 @@
+def tickets(people)
+    bills = { 25 => 0, 50 => 0, 100 => 0 }
+    
+    people.each { |n|
+      if n == 25
+        bills[25] += 1
+      elsif n == 50
+        if bills[25] >= 1
+          bills[25] -= 1
+        else
+          return 'NO'
+        end
+        
+        bills[50] += 1
+      else
+        if (bills[25] >= 1 and bills[50] >= 1)
+          bills[25] -= 1
+          bills[50] -= 1
+          bills[100] += 1
+        elsif (bills[25] >= 3)
+          bills[25] -= 3
+          bills[100] += 1
+        else
+          return 'NO'
+        end
+      end
+    }
+    
+    return 'YES'
+end
