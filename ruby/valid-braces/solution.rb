740f6f41ee68f3a434941d1a7702bde4602f3af2
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-17 22:16:56 -0300

    Add Valid Braces challenge

---
+++ ruby/valid-braces/solution.rb
@@ -0,0 +1,36 @@
+def validBraces(braces)
+    last_opened  = []
+    
+    braces.split('').each{ |c|
+      if c == '('
+        last_opened.push(c)
+      elsif (c == ')')
+        lo_size = last_opened.length
+        if lo_size == 0 or last_opened[lo_size - 1] != '('
+          return false
+        else
+          last_opened.pop
+        end
+      elsif c == '['
+        last_opened.push(c)
+      elsif (c == ']')
+        lo_size = last_opened.length
+        if lo_size == 0 or last_opened[lo_size - 1] != '['
+          return false
+        else
+          last_opened.pop
+        end
+      elsif c == '{'
+        last_opened.push(c)
+      elsif (c == '}')
+        lo_size = last_opened.length
+        if lo_size == 0 or last_opened[lo_size - 1] != '{'
+          return false
+        else
+          last_opened.pop
+        end
+      end
+    }
+  
+    return last_opened.length == 0
+end
