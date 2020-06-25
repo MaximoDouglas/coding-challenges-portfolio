9fa4db494c7f429058b81e53e9ac9730b411cd2d
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-24 21:05:00 -0300

    Add 'Replace With Alphabet Position' challenge

---
+++ ruby/replace-with-alphabet-position/solution.rb
@@ -0,0 +1,20 @@
+def alphabet_position(text)
+    indexes = ('a'..'z').each_with_index.map{|l,i| [l, i+1]}.to_h
+    
+    string_return = ""
+    letters = text.split('')
+  
+    letters.each_with_index{ |c, index|
+      position = indexes[c.downcase]
+  
+      if position
+        if string_return.length > 0 and index < letters.length
+          string_return << " "
+        end
+  
+        string_return << position.to_s
+      end
+    }
+    
+    return string_return
+end
