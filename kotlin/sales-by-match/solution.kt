327d42ed741868b831aea5388e89f5124d99b340
MaximoDouglas <douglas.h.maximo@gmail.com>
2021-09-24 23:59:06 -0300

    Add [Sales by Match] solution

---
+++ kotlin/sales-by-match/solution.kt
@@ -0,0 +1,16 @@
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    var colorsMap = HashMap<Int, Int> ()
+    
+    ar.forEach { color ->
+        val alreadyInMap = colorsMap.getOrDefault(color, 0)
+        colorsMap.put(color, alreadyInMap + 1)
+    }
+    
+    var amountOfPairs = 0
+    
+    colorsMap.forEach { _, amount ->
+        amountOfPairs += ((amount - amount%2)/2).toInt()
+    }
+
+    return amountOfPairs
+}
