efcaf242ed21b09c55da4afe46f24601c5bad2d2
Douglas Maximo <douglas.h.maximo@gmail.com>
2022-10-12 09:57:51 -0300

    Update and rename ruby/find-the-missing-letter/solution.rb to ruby/find-the-missing-letter.rb

--- ruby/find-the-missing-letter.rb
+++ ruby/find-the-missing-letter.rb
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+
 def get_alpha()
     ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   end
