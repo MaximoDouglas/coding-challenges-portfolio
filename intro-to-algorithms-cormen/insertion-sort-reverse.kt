3d7f0eca8959293a9154550b865978f120aded3f
MaximoDouglas <douglas.h.maximo@gmail.com>
2023-11-07 22:44:04 -0300

    Criar diretorio dedicado ao livro do cormen

---
+++ intro-to-algorithms-cormen/insertion-sort-reverse.kt
@@ -0,0 +1,23 @@
+val listOfIntegers = mutableListOf(5, 2, 4, 6, 1, 3)
+
+fun insertionSort(list: MutableList<Int>): List<Int> {
+    val n = list.size
+
+    for (i in 1 until n) {
+        val key = list[i]
+        var j = i - 1
+
+        while (j >= 0 && list[j] < key) {
+            list[j+1] = list[j]
+            j--
+        }
+
+        list[j+1] = key
+    }
+
+    return list
+}
+
+fun main() {
+    println(insertionSort(listOfIntegers))
+}
